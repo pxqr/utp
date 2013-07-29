bc7282d3b73b3ddef38813ab608c63da86616c58
Sam T <pxqr.sta@gmail.com>
2013-07-29 07:55:35 +0400

    ~ Use default extensions.

--- src/Network/UTP/Packet.hs
+++ src/Network/UTP/Packet.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE RecordWildCards #-}
 module Network.UTP.Packet  where
 
 import Control.Applicative
--- src/Network/UTP/Socket.hs
+++ src/Network/UTP/Socket.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE RecordWildCards #-}
 module Network.UTP.Socket where
 
 import Control.Applicative
